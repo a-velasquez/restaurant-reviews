8b528d5a8cf4e34c393adc9df690fb5ad3e4e3bd
Alex Velazquez <avelasquez075@gmail.com>
2021-07-25 18:47:58 -0700

    add DB connection, DB URI, catch error then async app.listen

--- backend/index.js
+++ backend/index.js
@@ -5,3 +5,19 @@ dotenv.config()
 const MongoClient = mongodb.MongoClient
 
 const port = process.env.PORT || 8000
+
+// DB Connection
+MongoClient.connect(process.env.RESTREVIEWS_DB_URI, {
+	poolSize: 50,
+	wtimeout: 2500,
+	useNewUrlParse: true
+})
+	.catch((err) => {
+		console.error(err.stack)
+		process.exit(1)
+	})
+	.then(async (client) => {
+		app.listen(port, () => {
+			console.log(`listening on port ${port}`)
+		})
+	})
