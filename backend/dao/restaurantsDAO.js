ae1c80d2b3ed45ae14f648d499858ba1ba363777
Alex Velazquez <avelasquez075@gmail.com>
2021-09-23 13:05:07 -0700

    npm install nodemon

--- backend/dao/restaurantsDAO.js
+++ backend/dao/restaurantsDAO.js
@@ -1,6 +1,5 @@
-import mongodb from 'mongodb'
+import mongodb from "mongodb"
 const ObjectId = mongodb.ObjectID
-
 let restaurants
 
 export default class RestaurantsDAO {
@@ -11,7 +10,7 @@ export default class RestaurantsDAO {
 		try {
 			restaurants = await conn
 				.db(process.env.RESTREVIEWS_NS)
-				.collection('restaurants')
+				.collection("restaurants")
 		} catch (e) {
 			console.error(
 				`Unable to establish a collection handle in restaurantsDAO: ${e}`
@@ -26,12 +25,12 @@ export default class RestaurantsDAO {
 	} = {}) {
 		let query
 		if (filters) {
-			if ('name' in filters) {
-				query = { $text: { $search: filters['name'] } }
-			} else if ('cuisine' in filters) {
-				query = { cuisine: { $eq: filters['cuisine'] } }
-			} else if ('zipcode' in filters) {
-				query = { 'address.zipcode': { $eq: filters['zipcode'] } }
+			if ("name" in filters) {
+				query = { $text: { $search: filters["name"] } }
+			} else if ("cuisine" in filters) {
+				query = { cuisine: { $eq: filters["cuisine"] } }
+			} else if ("zipcode" in filters) {
+				query = { "address.zipcode": { $eq: filters["zipcode"] } }
 			}
 		}
 
@@ -58,6 +57,60 @@ export default class RestaurantsDAO {
 				`Unable to convert cursor to array or problem counting documents, ${e}`
 			)
 			return { restaurantsList: [], totalNumRestaurants: 0 }
+		}
+	}
+	static async getRestaurantByID(id) {
+		try {
+			const pipeline = [
+				{
+					$match: {
+						_id: new ObjectId(id)
+					}
+				},
+				{
+					$lookup: {
+						from: "reviews",
+						let: {
+							id: "$_id"
+						},
+						pipeline: [
+							{
+								$match: {
+									$expr: {
+										$eq: ["$restaurant_id", "$$id"]
+									}
+								}
+							},
+							{
+								$sort: {
+									date: -1
+								}
+							}
+						],
+						as: "reviews"
+					}
+				},
+				{
+					$addFields: {
+						reviews: "$reviews"
+					}
+				}
+			]
+			return await restaurants.aggregate(pipeline).next()
+		} catch (e) {
+			console.error(`Something went wrong in getRestaurantByID: ${e}`)
+			throw e
+		}
+	}
+
+	static async getCuisines() {
+		let cuisines = []
+		try {
+			cuisines = await restaurants.distinct("cuisine")
+			return cuisines
+		} catch (e) {
+			console.error(`Unable to get cuisines, ${e}`)
+			return cuisines
 		}
 	}
 }
--- backend/dao/reviewsDAO.js
+++ backend/dao/reviewsDAO.js
@@ -1,4 +1,4 @@
-import mongodb from 'mongodb'
+import mongodb from "mongodb"
 const ObjectId = mongodb.ObjectID
 
 let reviews
@@ -9,7 +9,7 @@ export default class ReviewsDAO {
 			return
 		}
 		try {
-			reviews = await conn.db(process.env.RESTREVIEWS_NS).collection('reviews')
+			reviews = await conn.db(process.env.RESTREVIEWS_NS).collection("reviews")
 		} catch (e) {
 			console.error(`Unable to establish collection handles in userDAO: ${e}`)
 		}
