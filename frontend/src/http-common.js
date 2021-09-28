da2257cc8b550729b6e0c4136407cdfed66e7749
Alex Velazquez <avelasquez075@gmail.com>
2021-09-27 20:43:58 -0700

    implement realm serverless backend

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 export default axios.create({
-	baseURL: "http://localhost:5000/api/v1/restaurants",
+	baseURL:
+		"https://us-west-2.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/restaurant-reviews-vdmcv/service/restaurants/incoming_webhook/",
 	headers: {
 		"Content-type": "application/json"
 	}
