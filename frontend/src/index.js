550a7cbdedeb4a69a3817b9a5a8a10d258364f57
Alex Velazquez <avelasquez075@gmail.com>
2021-10-17 23:38:35 -0700

    add npm i dotenv

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
 import { BrowserRouter } from "react-router-dom"
+import { Auth0Provider } from "@auth0/auth0-react"
 
 ReactDOM.render(
 	<BrowserRouter>
