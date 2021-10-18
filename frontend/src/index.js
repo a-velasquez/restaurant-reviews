59a085517fe06685364bfbd4f8d93abf7d9b4116
Alex Velazquez <avelasquez075@gmail.com>
2021-10-18 13:50:03 -0700

    wrap app in Auth0 Provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,8 +5,14 @@ import { BrowserRouter } from "react-router-dom"
 import { Auth0Provider } from "@auth0/auth0-react"
 
 ReactDOM.render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>,
+	<Auth0Provider
+		domain='dev-8z5t2mgw.us.auth0.com'
+		clientId='nNtNm4p0XYk1j7ILeUMHYAexFU21qlZ0'
+		redirectUri={window.location.origin}
+		cacheLocation='localstorage'>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Auth0Provider>,
 	document.getElementById("root")
 )
